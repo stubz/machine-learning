e632b0da6278c951ec7e345a8c3e17e3fc99ca03
stubz <yamamoto.yusuke+github@gmail.com>
2017-02-08 23:48:14 +0900

    implemented get_maxQ

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -78,7 +78,7 @@ class LearningAgent(Agent):
         ###########
         # Calculate the maximum Q-value of all actions for a given state
 
-        maxQ = None
+        maxQ = max(self.Q[state].values())
 
         return maxQ 
 
